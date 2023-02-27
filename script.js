d3f271673d73607ffe96d271d839642b5c7dd94c
Edyta Kawala <edyta.wer.kawala@gmail.com>
2023-02-27 19:31:28 +0100

    Create sum, subtract, divide, multiply and operate functions

---
+++ script.js
@@ -0,0 +1,32 @@
+function sumAll(input, number){ 
+    input += number;
+    return input;
+};
+
+function subAll(input, number){
+    input -= number;
+    return input;
+}
+function mulAll(input, number){
+    input *= number;
+    return input;
+}
+function divAll(input, number){
+    input /= number;
+    return input;  
+}
+
+function operate(operator, firstNumber, secondNumber){
+    switch(operator){
+        case '+':
+             return sumAll(firstNumber,secondNumber);
+        case '-':
+            return subAll(firstNumber,secondNumber);
+        case '*':
+            return mulAll(firstNumber,secondNumber);
+        case '/':
+            return divAll(firstNumber,secondNumber);
+
+    }
+};
+console.log(operate('/', 3,2));
