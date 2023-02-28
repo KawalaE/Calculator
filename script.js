46a501cd7b629cc47736eb4e0e8403979df1ed93
Edyta Kawala <edyta.wer.kawala@gmail.com>
2023-02-28 21:08:14 +0100

    Display numbers via buttons

--- script.js
+++ script.js
@@ -29,4 +29,82 @@ function operate(operator, firstNumber, secondNumber){
 
     }
 };
-console.log(operate('', 3,2));
+//console.log(operate('', 3,2));
+
+let currentNumberArray = [];
+let currentNumber = ""; 
+
+let inputNumbers = document.createElement('div');
+inputNumbers.classList.add('display-font');
+let displayScreen = document.querySelector('.display')
+displayScreen.appendChild(inputNumbers);
+
+let seven = document.querySelector('.seven');
+seven.addEventListener('click', () => {
+    currentNumberArray.push(7);
+    currentNumber = currentNumberArray.join("");
+    inputNumbers.textContent = `${currentNumber}`;
+});
+
+let eight = document.querySelector('.eight');
+eight.addEventListener('click', () => {
+    currentNumberArray.push(8);
+    currentNumber = currentNumberArray.join("");
+    inputNumbers.textContent = `${currentNumber}`;
+});
+
+let nine = document.querySelector('.nine');
+nine.addEventListener('click', () => {
+    currentNumberArray.push(9);
+    currentNumber = currentNumberArray.join("");
+    inputNumbers.textContent = `${currentNumber}`;
+});
+
+let four = document.querySelector('.four');
+four.addEventListener('click', () => {
+    currentNumberArray.push(4);
+    currentNumber = currentNumberArray.join("");
+    inputNumbers.textContent = `${currentNumber}`;
+});
+
+let five = document.querySelector('.five');
+five.addEventListener('click', () => {
+    currentNumberArray.push(5);
+    currentNumber = currentNumberArray.join("");
+    inputNumbers.textContent = `${currentNumber}`;
+});
+
+let six = document.querySelector('.six');
+six.addEventListener('click', () => {
+    currentNumberArray.push(6);
+    currentNumber = currentNumberArray.join("");
+    inputNumbers.textContent = `${currentNumber}`;
+});
+
+let three = document.querySelector('.three');
+three.addEventListener('click', () => {
+    currentNumberArray.push(3);
+    currentNumber = currentNumberArray.join("");
+    inputNumbers.textContent = `${currentNumber}`;
+});
+
+let two = document.querySelector('.two');
+two.addEventListener('click', () => {
+    currentNumberArray.push(2);
+    currentNumber = currentNumberArray.join("");
+    inputNumbers.textContent = `${currentNumber}`;
+});
+
+let one = document.querySelector('.one');
+one.addEventListener('click', () => {
+    currentNumberArray.push(1);
+    currentNumber = currentNumberArray.join("");
+    inputNumbers.textContent = `${currentNumber}`;
+});
+
+let zero = document.querySelector('.zero');
+zero.addEventListener('click', () => {
+    currentNumberArray.push(0);
+    currentNumber = currentNumberArray.join("");
+    inputNumbers.textContent = `${currentNumber}`;
+});
