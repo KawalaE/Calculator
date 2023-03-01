56d2d189709bbd7e9ca24603525a84084fb1a9c2
Edyta Kawala <edyta.wer.kawala@gmail.com>
2023-03-01 13:42:29 +0100

    Fix dot operator

--- script.js
+++ script.js
@@ -40,104 +40,55 @@ let displayScreen = document.querySelector('.display')
 displayScreen.appendChild(inputNumbers);
 
 let clear = document.querySelector('.clear');
-clear.addEventListener('click', () => {
-    currentNumberArray = [];
-    currentNumber = "";
-    inputNumbers.textContent = `${currentNumber}`;
-});
-
 let remove = document.querySelector('.remove');
-remove.addEventListener('click', () => {
-    currentNumberArray.pop();
-    currentNumber = currentNumber.slice(0,-1);
-    inputNumbers.textContent = `${currentNumber}`;
-});
 
 let seven = document.querySelector('.seven');
-seven.addEventListener('click', () => {
-    currentNumberArray.push(7);
-    currentNumber = currentNumberArray.join("");
-    inputNumbers.textContent = `${currentNumber}`;
-    
-});
-
 let eight = document.querySelector('.eight');
-eight.addEventListener('click', () => {
-    currentNumberArray.push(8);
-    currentNumber = currentNumberArray.join("");
-    inputNumbers.textContent = `${currentNumber}`;
-    
-});
-
 let nine = document.querySelector('.nine');
-nine.addEventListener('click', () => {
-    currentNumberArray.push(9);
-    currentNumber = currentNumberArray.join("");
-    inputNumbers.textContent = `${currentNumber}`;
-    
-});
-
 let four = document.querySelector('.four');
-four.addEventListener('click', () => {
-    currentNumberArray.push(4);
-    currentNumber = currentNumberArray.join("");
-    inputNumbers.textContent = `${currentNumber}`;
-    
-});
-
 let five = document.querySelector('.five');
-five.addEventListener('click', () => {
-    currentNumberArray.push(5);
-    currentNumber = currentNumberArray.join("");
-    inputNumbers.textContent = `${currentNumber}`;
-    
-});
-
 let six = document.querySelector('.six');
-six.addEventListener('click', () => {
-    currentNumberArray.push(6);
-    currentNumber = currentNumberArray.join("");
-    inputNumbers.textContent = `${currentNumber}`;
-    
-});
-
 let three = document.querySelector('.three');
-three.addEventListener('click', () => {
-    currentNumberArray.push(3);
-    currentNumber = currentNumberArray.join("");
-    inputNumbers.textContent = `${currentNumber}`;
-    
-});
-
 let two = document.querySelector('.two');
-two.addEventListener('click', () => {
-    currentNumberArray.push(2);
-    currentNumber = currentNumberArray.join("");
-    inputNumbers.textContent = `${currentNumber}`;
-    
-});
-
 let one = document.querySelector('.one');
-one.addEventListener('click', () => {
-    currentNumberArray.push(1);
-    currentNumber = currentNumberArray.join("");
-    inputNumbers.textContent = `${currentNumber}`;
-    
-});
-
 let zero = document.querySelector('.zero');
-zero.addEventListener('click', () => {
-    currentNumberArray.push(0);
-    currentNumber = currentNumberArray.join("");
-    inputNumbers.textContent = `${currentNumber}`;
-    
-});
-
 let dot = document.querySelector('.dot');
+
+function choiceHandler(buttonChoice, number){
+    if (buttonChoice === clear){
+        currentNumberArray = [];
+        currentNumber = "";
+        inputNumbers.textContent = `${currentNumber}`;
+    }
+    if (buttonChoice === remove){
+        currentNumberArray.pop();
+        currentNumber = currentNumber.slice(0,-1);
+        inputNumbers.textContent = `${currentNumber}`;
+    }
+    else {
+        currentNumberArray.push(number);
+        currentNumber = currentNumberArray.join("");
+        inputNumbers.textContent = `${currentNumber}`;
+
+    }
+}
+
+clear.addEventListener('click', () => choiceHandler(clear));
+remove.addEventListener('click', () =>  choiceHandler(remove));
+seven.addEventListener('click', () => choiceHandler(seven, 7));
+eight.addEventListener('click', () => choiceHandler(eight, 8));
+nine.addEventListener('click', () => choiceHandler(nine, 9));
+four.addEventListener('click', () => choiceHandler(four, 4));
+five.addEventListener('click', () => choiceHandler(five, 5));
+six.addEventListener('click', () => choiceHandler(six, 6 ));
+three.addEventListener('click', () => choiceHandler(three, 3));
+two.addEventListener('click', () => choiceHandler(two, 2));
+one.addEventListener('click', () => choiceHandler(one, 1));
+zero.addEventListener('click', () => choiceHandler(zero, 0));
 dot.addEventListener('click', () => {
-    currentNumberArray.push('.');
-    currentNumber = currentNumberArray.join("");
-    inputNumbers.textContent = `${currentNumber}`;
-    
+    if(currentNumberArray.length == 0 && !currentNumberArray.includes(".") && !currentNumberArray.includes("0.")){
+        choiceHandler(dot, "0.");
+    } else if(!currentNumberArray.includes(".") && !currentNumberArray.includes("0.")){choiceHandler(dot,".")}
+
 });
 
