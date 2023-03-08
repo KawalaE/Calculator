ffa7c183dc5a40207bab450521c1548d71f8f296
Edyta Kawala <edyta.wer.kawala@gmail.com>
2023-03-08 22:58:58 +0100

    Fix operator handler function

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let currentNumberArray = [];
+let numbers = [];
+let lastOperator;
+let calculateValue = 0;
+let operatorUsed = 0;
+let equalPressed = false;
+let displayContent = document.createElement('div');
+displayContent.classList.add('display-font');
+let displayScreen = document.querySelector('.display')
+displayScreen.appendChild(displayContent);
+
 function sumAll(input, number){ 
     input += number;
     return input;
@@ -33,30 +44,17 @@ function operate(operator, firstNumber, secondNumber){
     }
 };
 
-let currentNumberArray = [];
-let currentNumber = ""; 
-let numbers = [];
-let lastOperator;
-let calculateValue = 0;
-let operatorUsed = 0;
-let equalPressed = 0;
-let inputNumbers = document.createElement('div');
-inputNumbers.classList.add('display-font');
-let displayScreen = document.querySelector('.display')
-displayScreen.appendChild(inputNumbers);
-
 function evaluateEquation(){
 
     if(numbers.length == 2){
-        console.log(numbers[0]);
-        console.log(numbers[1]);
         calculateValue = operate(lastOperator, numbers[0], numbers[1]);
         let newValue = (String(calculateValue).split(''));
+        let ble = newValue.slice(0,newValue.indexOf('.')).join('');
         let decimalPoints;
 
         if(newValue.includes('.')){
             decimalPoints = newValue.slice(newValue.indexOf('.') +1);
-            console.log(decimalPoints)
+
             for(let i=decimalPoints.length-1; i>=0; i--){
                 if(decimalPoints[i] == 0){
                     decimalPoints.pop()
@@ -64,8 +62,8 @@ function evaluateEquation(){
             }
 
             decimalPoints = decimalPoints.join('');
-            calculateValue = Number(`${newValue.slice(0,newValue.indexOf('.')).join('')}.${decimalPoints}`);
-            
+            calculateValue = Number(`${ble}.${decimalPoints}`);
+
         }
 
         if ((String(calculateValue).split('')).length > 8){
@@ -79,7 +77,7 @@ function evaluateEquation(){
             calculateValue = Number(calculateValue.join(''));
         } 
 
-        inputNumbers.textContent = `${calculateValue}`;
+        displayContent.textContent = `${calculateValue}`;
         numbers.pop();
         numbers[0] = calculateValue;
         operatorUsed =0;
@@ -88,37 +86,42 @@ function evaluateEquation(){
 function removeNumber(){
     if(currentNumberArray.length){
         currentNumberArray.pop();
-        currentNumber = currentNumberArray.join('');
-        inputNumbers.textContent = `${currentNumber}`;
+        displayContent.textContent = `${currentNumberArray.join('')}`;
     }
 }
 function displayClear(){
-    equalPressed = 0;
+    equalPressed = false;
     currentNumberArray.length = 0;
-    currentNumber = "";
-    inputNumbers.textContent = `${currentNumber}`;
+    displayContent.textContent = `${currentNumberArray.join('')}`;
 }
 
 function operatorHandler(operatorSymbol){
-    if(equalPressed != 1){
+ 
+    if(equalPressed == true && numbers[0] != Number(displayContent.textContent)){
+        numbers[0] = (Number(currentNumberArray.join("")));
+    }
+    if(equalPressed == false && numbers.length == 1) {
         numbers.push(Number(currentNumberArray.join("")));
     }
+    if(numbers.length == false){
+        numbers.push(Number(currentNumberArray.join("")));
+    }
+    equalPressed = false;
     displayClear();
     evaluateEquation();
+    operatorUsed = 1;
     lastOperator = operatorSymbol;
 }
 
-function choiceHandler(number){
+
+function numberHandler(number){
     /*Limit the amount of numbers visible at the display*/ 
     if(currentNumberArray.length <= 9){
-
         if(currentNumberArray[0] == "0" && currentNumberArray.length == 1 && !(number ==".") && !(number =='0.')){
             currentNumberArray.shift();
         }
         currentNumberArray.push(number);
-        currentNumber = currentNumberArray.join("");
-        inputNumbers.textContent = `${currentNumber}`;
-        
+        displayContent.textContent = `${currentNumberArray.join('')}`;    
     } 
 }
 
@@ -128,49 +131,37 @@ equalSignButton.addEventListener('click', () => {
     if(operatorUsed){
         numbers.push(Number(currentNumberArray.join("")));
         currentNumberArray.length = 0;
-        evaluateEquation();
-        equalPressed = 1
-    } else{
-        currentNumberArray.length = 0;
-
+        evaluateEquation();   
     }
+    equalPressed = true;
 })
 
 /* Get the nodelist of all button-operators */
 let operatorButtons = document.querySelectorAll('.operator')
-
-operatorButtons.forEach((operButton) => {
-    operButton.addEventListener('click', () => {
-        operatorUsed =1;
-        let getOperator = operButton.textContent;
+operatorButtons.forEach((operatorButton) => {
+    operatorButton.addEventListener('click', () => {
+        let getOperator = operatorButton.textContent;
         if(getOperator == '.'){
             if(currentNumberArray.length == 0 && !currentNumberArray.includes(".") && !currentNumberArray.includes("0.")){
-                choiceHandler("0.");
-            } else if(!currentNumberArray.includes(".") && !currentNumberArray.includes("0.") ){choiceHandler(".")}
+                numberHandler("0.");
+            } else if(!currentNumberArray.includes(".") && !currentNumberArray.includes("0.") ){numberHandler(".")}
         } else {
             operatorHandler(getOperator);
         }
-        
     })
 })
 
 /* Get the nodelist of all button-numbers */
 let numberButtons = document.querySelectorAll('.number');
-
 numberButtons.forEach((numButton)=>{
     numButton.addEventListener('click', () => {
         let getNumber = Number(numButton.textContent);
-        if (equalPressed == 1){
-            numbers.length = 1;
-            numbers[0]=getNumber;
-        }
-        choiceHandler(getNumber);
-        
+        numberHandler(getNumber);
     })
 })
 
+/* Get the nodelist and evaluate CA and CE buttons*/
 let additionalOperations = document.querySelectorAll('.additional-options') ;
-
 additionalOperations.forEach((addOperation) => {
     addOperation.addEventListener('click', () => {
         let getOperation = addOperation.textContent;
@@ -180,5 +171,4 @@ additionalOperations.forEach((addOperation) => {
         }if(getOperation == 'CE'){
             removeNumber();
         }})
-    
 })
